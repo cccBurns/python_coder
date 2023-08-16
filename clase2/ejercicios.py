fcfe302b9ad4786e36bf5c5143b9007ef2eb67c7
christian burns <christianburns03@gmail.com>
2023-08-16 09:46:41 -0300

    clase 2 final

--- clase2/ejercicios.py
+++ clase2/ejercicios.py
@@ -7,4 +7,9 @@ lista1.append(456789)
 lista1.append("hola mundo")
 
 lista1[6:] = [456789, 'hola mundo']
+
 print(lista1)
+
+# LINK DE EJERCICIOS: https://colab.research.google.com/drive/13BgVhUIH9FH8iljVBynax1g4YmjrvmkM?usp=sharing#scrollTo=OLH5FHRK369B
+
+# OTRO LINK REPO: https://github.com/CoderContenidos/Python-V4
